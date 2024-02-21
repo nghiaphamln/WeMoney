3175ef05bcbb72df048c9480c211cf8567a13972
Minh Nghĩa <nghiaphamln3@gmail.com>
2024-02-21 13:02:23 +0700

    refactor middleware add log stack trace

---
+++ Backend/WeMoney/WeMoney/Helper/ExceptionExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace WeMoney.Helper;
+
+public static class ExceptionExtension
+{
+    public static string? GetTrimmedStackTrace(this string stackTrace)
+    {
+        var lines = stackTrace.Split('\n');
+        var relevantLines = lines.Where(line => line.Contains(".cs:") || line.Contains(".vb:"));
+        return string.Join('\n', relevantLines);
+    }
+}
--- Backend/WeMoney/WeMoney/Middleware/RequestTimingMiddleware.cs
+++ Backend/WeMoney/WeMoney/Middleware/RequestTimingMiddleware.cs
@@ -1,8 +1,8 @@
 ﻿using System.Diagnostics;
 using System.Net;
 using System.Text;
-using Microsoft.IdentityModel.Tokens;
 using ServiceStack;
+using WeMoney.Helper;
 using WeMoney.Models.Base;
 
 namespace WeMoney.Middleware;
@@ -16,32 +16,37 @@ public class RequestTimingMiddleware(
     {
         var stopwatch = Stopwatch.StartNew();
         var stringBuilder = new StringBuilder();
+        Exception? exception = null;
         try
         {
             await next(context);
         }
         catch (Exception e)
         {
-            stringBuilder.Append($"Exception: {e.Message} ");
+            exception = e;
         }
         finally
         {
             stopwatch.Stop();
             var elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
-            var error = stringBuilder.ToString();
-            if (CollectionUtilities.IsNullOrEmpty(error))
+            if (exception is not null)
             {
-                logger.LogInformation("[{ProcessTime}ms] [{Status}] {RequestPath}", 
-                    elapsedMilliseconds, context.Response.StatusCode, context.Request.Path);
+                var response = new BaseResponse(exception.Message).ToJson();
+                stringBuilder.AppendLine();
+                stringBuilder.AppendLine($"Exception: {exception.Message} ");
+                stringBuilder.AppendLine($"StackTrace: {exception.StackTrace?.GetTrimmedStackTrace()} ");
+                
+                logger.LogCritical("[{ProcessTime}ms] [{Status}] {RequestPath} {Error}",
+                    elapsedMilliseconds, context.Response.StatusCode, context.Request.Path, stringBuilder.ToString());
+                
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(response);
             }
             else
             {
-                logger.LogCritical("[{ProcessTime}ms] [{Status}] {RequestPath} {Error}", 
-                    elapsedMilliseconds, context.Response.StatusCode, context.Request.Path, error);
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                context.Response.ContentType = "application/json";
-                var response = new BaseResponse(error).ToJson();
-                await context.Response.WriteAsync(response);
+                logger.LogInformation("[{ProcessTime}ms] [{Status}] {RequestPath}",
+                    elapsedMilliseconds, context.Response.StatusCode, context.Request.Path);
             }
         }
     }
